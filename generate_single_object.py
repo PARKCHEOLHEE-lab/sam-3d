aaec76ce15871bd8dda4282fc3485183d920d742
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2026-01-16 08:47:00 +0000

    maek a video, fix misprint

--- generate_single_object.py
+++ generate_single_object.py
@@ -2,13 +2,17 @@
 import os
 import sys
 import pytz
+import imageio
 import datetime
 import argparse
 import PIL.Image
 
+from loguru import logger
+
+
 sys.path.append("notebook")
 
-from inference import Inference, load_image, load_single_mask
+from inference import Inference, load_image, load_single_mask, make_scene, ready_gaussian_for_video_rendering, render_video
 
 
 def parse_args() -> argparse.Namespace:
@@ -28,10 +32,18 @@ def parse_args() -> argparse.Namespace:
         type=str, 
         default="output"
     )
+    parser.add_argument(
+        "--gif",
+        type=str,
+        default="false"
+    )
     return parser.parse_args()
 
 
 def generate(args: argparse.Namespace) -> None:
+    
+    assert args.gif in ["true", "false"]
+    args.gif = args.gif == "true"
     
     # make output directory
     timestamp = datetime.datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y%m%d_%H%M%S")
@@ -49,14 +61,40 @@ def generate(args: argparse.Namespace) -> None:
     mask = load_single_mask(os.path.dirname(args.image_path), index=args.mask_index)
     masked_image = image.copy()
     masked_image[mask == 0] = 0
-    PIL.Image.fromarray(masked_image).save(os.path.join(output_dir, f"_{args.mask_index:03d}.png"))
+    PIL.Image.fromarray(masked_image).save(os.path.join(output_dir, f"_masked_{args.mask_index:03d}.png"))
 
     # run model
-    output = inference(masked_image, mask, seed=42)
+    logger.info(f"Running model with image and mask...")
+    output = inference(image, mask, seed=42)
 
     # export gaussian splat and mesh
+    logger.info(f"Exporting gaussian splat and mesh...")
     output["gs"].save_ply(os.path.join(output_dir, "splat.ply"))
     output["glb"].export(os.path.join(output_dir, "mesh.glb"))
+    
+    if args.gif:
+        # make a video
+        logger.info(f"Making a video...")
+        scene_gs = make_scene(output)
+        scene_gs = ready_gaussian_for_video_rendering(scene_gs)
+
+        video = render_video(
+            scene_gs,
+            r=1,
+            fov=60,
+            pitch_deg=15,
+            yaw_start_deg=-45,
+            resolution=512,
+        )["color"]
+
+        # save video as gif
+        imageio.mimsave(
+            os.path.join(output_dir, "_video.gif"),
+            video,
+            format="GIF",
+            duration=1000 / 30,  # default assuming 30fps from the input MP4
+            loop=0,  # 0 means loop indefinitely
+        )
 
 
 if __name__ == "__main__":
