ee349331f55bb369b295c9dd5e307b513ca77706
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2026-01-20 05:02:11 +0000

    merged scene

--- main_inference.py
+++ main_inference.py
@@ -2,12 +2,15 @@
 import os
 import sys
 import pytz
+import torch
 import imageio
 import datetime
 import argparse
 import PIL.Image
 
 from loguru import logger
+from copy import deepcopy
+from pytorch3d.transforms import quaternion_multiply, quaternion_invert, quaternion_to_matrix
 
 sys.path.append("notebook")
 
@@ -18,7 +21,8 @@ from inference import (
     load_masks,
     make_scene, 
     ready_gaussian_for_video_rendering,
-    render_video
+    render_video,
+    SceneVisualizer
 )
 
 
@@ -154,6 +158,66 @@ def generate_single_object(args: argparse.Namespace, output_path: str, use_infer
 def generate_multi_object(args: argparse.Namespace, output_path: str, use_inference_cache: bool = False) -> None:
     # TODO: implement multi object inference to combine multiple objects into a single scene
     # https://github.com/facebookresearch/sam-3d-objects/issues/36
+
+
+    def _process_output(output: dict, in_place: bool = False, minimum_kernel_size: float = float("inf")) -> dict:
+        if not in_place:
+            output = deepcopy(output)
+
+        rotation = output["rotation"]
+        translation = output["translation"]
+        scale = output["scale"]
+            
+        # process gaussian
+        # move gaussians to scene frame of reference
+        PC = SceneVisualizer.object_pointcloud(
+            points_local=output["gaussian"][0].get_xyz.unsqueeze(0),
+            quat_l2c=rotation,
+            trans_l2c=translation,
+            scale_l2c=scale,
+        )
+        output["gaussian"][0].from_xyz(PC.points_list()[0])
+        # must ... ROTATE
+        output["gaussian"][0].from_rotation(
+            quaternion_multiply(
+                quaternion_invert(rotation),
+                output["gaussian"][0].get_rotation,
+            )
+        )
+        scale = output["gaussian"][0].get_scaling
+        adjusted_scale = scale * scale
+        assert (
+            scale[0, 0].item()
+            == scale[0, 1].item()
+            == scale[0, 2].item()
+        )
+        output["gaussian"][0].mininum_kernel_size *= scale[0, 0].item()
+        adjusted_scale = torch.maximum(
+            adjusted_scale,
+            torch.tensor(
+                output["gaussian"][0].mininum_kernel_size * 1.1,
+                device=adjusted_scale.device,
+            ),
+        )
+        output["gaussian"][0].from_scaling(adjusted_scale)
+        minimum_kernel_size = min(
+            minimum_kernel_size,
+            output["gaussian"][0].mininum_kernel_size,
+        )
+        
+        # process glb
+        glb: trimesh.Trimesh
+        glb = output["glb"]
+        
+        vertices_torch = torch.from_numpy(np.asarray(glb.vertices)).to(rotation.device).to(rotation.dtype)
+        vertices_torch = vertices_torch * scale
+        vertices_torch = vertices_torch @ quaternion_to_matrix(rotation)
+        vertices_torch = vertices_torch + translation
+        
+        glb.vertices = vertices_torch.detach().cpu().numpy()
+        output["glb"] = glb
+
+        return output
     
     # load model
     if use_inference_cache:
@@ -166,16 +230,39 @@ def generate_multi_object(args: argparse.Namespace, output_path: str, use_infere
     image = load_image(args.image_path)
     masks = load_masks(os.path.dirname(args.image_path), extension=".png")
     
-    # run model
-    outputs = [inference(image, mask, seed=42) for mask in masks]
-
-    for oi, output in enumerate(outputs):
-        # export gaussian splat and mesh
-        logger.info(f"Exporting gaussian splat and mesh for object {oi:03d}...")
-        output["gs"].save_ply(os.path.join(output_path, f"splat_{oi:03d}.ply"))
-        output["glb"].export(os.path.join(output_path, f"mesh_{oi:03d}.glb"))
-        logger.info(f"Gaussian splat and mesh exported for object {oi:03d}")
+    glb_scene = trimesh.Scene()
+    all_outputs = []
+    minimum_kernel_size = float("inf")
+    for mask in masks[:1]:
+        # run model
+        output = inference(image, mask, seed=42)
+        output = _process_output(output, minimum_kernel_size=minimum_kernel_size)
+        minimum_kernel_size = min(
+            minimum_kernel_size,
+            output["gaussian"][0].mininum_kernel_size,
+        )
+        glb_scene.add_geometry(output["glb"], name=f"object_{mask_index:03d}")
+        all_outputs.append(output)
     
+    glb_scene.export(os.path.join(output_path, "_merged_scene.glb"))
+    logger.info(f"Merged scene exported as GLB")
+
+    # merge gaussians
+    scene_gs = all_outputs[0]["gaussian"][0]
+    scene_gs.mininum_kernel_size = minimum_kernel_size
+    for out in all_outputs[1:]:
+        out_gs = out["gaussian"][0]
+        scene_gs._xyz = torch.cat([scene_gs._xyz, out_gs._xyz], dim=0)
+        scene_gs._features_dc = torch.cat(
+            [scene_gs._features_dc, out_gs._features_dc], dim=0
+        )
+        scene_gs._scaling = torch.cat([scene_gs._scaling, out_gs._scaling], dim=0)
+        scene_gs._rotation = torch.cat([scene_gs._rotation, out_gs._rotation], dim=0)
+        scene_gs._opacity = torch.cat([scene_gs._opacity, out_gs._opacity], dim=0)
+
+    scene_gs.save_ply(os.path.join(output_path, "_merged_scene.ply"))
+    logger.info(f"Merged scene exported as PLY")
+
     if args.export_images:
         for mi, mask in enumerate(masks):
             masked_image = image.copy()
